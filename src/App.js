6ec0e03e457af66a6ae8080f0b8d7699293760ce
Harshit <harshit2001pandey@gmail.com>
2024-07-14 15:04:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import "./App.css";
+import DisplayCalc from "./Components/DisplayCalc";
+import ButtonDisplay from "./Components/ButtonDisplay";
 
 function App() {
+  const [calVal, setCalVal] = useState([""])
+  function onButtonClick(btnPassed){
+    if(btnPassed === "C"){
+      setCalVal("")
+    }
+    else if(btnPassed === "="){
+      let val = eval(calVal)
+      setCalVal(val)
+    }
+    else {
+    const newDisplayValue = calVal + btnPassed
+     setCalVal(newDisplayValue)
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <DisplayCalc displayValue={calVal}/>
+        <ButtonDisplay onButtonClick={onButtonClick}/>
+      </div>
+    </>
   );
 }
 
